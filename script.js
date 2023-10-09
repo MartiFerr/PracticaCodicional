5da2e6468a993776621a493eaf7aa618276a0233
alumno <alumno@Instituto.local>
2023-10-09 11:05:46 -0300

    cambio

---
+++ script.js
@@ -0,0 +1,11 @@
+let cantidadhijos = 5
+let edad = 2023 - 1994
+let catidaddecasas = 0
+let trabajoformal = true
+if ((edad>= 18) && (trabajoformal == true) && (cantidaddecasas > 0))
+{
+    alert ("Podes etrar al sorteo")
+}
+else {
+    alert ("No podes entraral sorteo")
+}
